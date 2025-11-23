3c4c20f7f3402f0fc917571b745359f03aa1cb2b
GLDNRMZ <122560170+GLDNRMZ@users.noreply.github.com>
2025-11-22 23:38:32 -0800

    Initial

---
+++ config.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.Keys = { 22 } --  Q (44) / SPACEBAR (22)
+--Config.Delay = 1 -- In Seconds
+Config.Webhook = 'https://discord.com/api/webhooks/1370860427902324736/Gpje3tUZieXlL1WY06RYHEV0_haWWF2u03XbtH9pp55C8YAjCKzTrQEd1EikyjLSr3US'
